24675815fc3e725fba346dd71da6c964649a2bad
James911015 <32137620+James911015@users.noreply.github.com>
2026-01-26 10:49:55 -0500

    feat: Add FlashcardModel for flashcard data representation and JSON serialization.

---
+++ lib/features/study_session/data/models/flashcard_model.dart
@@ -0,0 +1,36 @@
+import '../../domain/entities/flashcard.dart';
+
+class FlashcardModel extends Flashcard {
+  // 1. Constructor
+  // Simplemente recibe los datos y se los pasa al padre (super)
+  const FlashcardModel({
+    required super.imagePath,
+    required super.sourceWord,
+    required super.targetWord,
+    super.phonetic,
+  });
+
+  // 2. Factory: fromJson
+  // Este es el traductor. Convierte el Mapa (JSON) en un Objeto Dart.
+  factory FlashcardModel.fromJson(Map<String, dynamic> json) {
+    return FlashcardModel(
+      // Usamos los nombres exactos que vienen de tu API Spring Boot
+      // Agregamos '??' (null coalescing) para proteger la app si un campo viene nulo
+      imagePath: json['imagePath'],
+      sourceWord: json['sourceWord'],
+      targetWord: json['targetWord'],
+      phonetic: json['phonetic'],
+    );
+  }
+
+  // 3. Método: toJson (Opcional por ahora)
+  // Útil si alguna vez necesitas enviar datos DE VUELTA al servidor
+  Map<String, dynamic> toJson() {
+    return {
+      'imagePath': imagePath,
+      'sourceWord': sourceWord,
+      'targetWord': targetWord,
+      'phonetic': phonetic,
+    };
+  }
+}
